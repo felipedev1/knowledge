3f3e5b02d1595786e57e98ef00a7c9e23027eeec
Felipe <felipe.pereira.dev@gmail.com>
2020-05-02 20:36:26 -0300

    Soft Delete de Usuário

--- backend/api/user.js
+++ backend/api/user.js
@@ -38,6 +38,7 @@ module.exports = app => {
       app.db('users')
         .update(user)
         .where({ id: user.id })
+        .whereNull('deletedAt')
         .then(_ => res.status(204).send())
         .catch(err => res.status(500).send(err))
     } else {
@@ -52,6 +53,7 @@ module.exports = app => {
   const get = (req, res) => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
+      .whereNull('deletedAt')
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
@@ -59,11 +61,30 @@ module.exports = app => {
   const getById = (req, res) => {
     app.db('users')
       .where({ id: req.params.id })
+      .whereNull('deletedAt')
       .first()
       .select('id', 'name', 'email', 'admin')
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
 
-  return { save, get, getById }
+  const remove = async (req, res) => {
+    try {
+      const articles = await app.db('articles')
+        .where({ userId: req.params.id})
+      notExistsOrError(articles, 'Usuário possui artigos.')
+
+      const rowsUpdated = await app.db('users')
+        .update({deletedAt: new Date()})
+        .where({id: req.params.id})
+        .whereNull('deletedAt')
+      existsOrError(rowsUpdated, 'Usuário não encontrado.')
+
+      res.status(204).send()
+    } catch(msg){
+      res.send(500)
+    }
+  }
+
+  return { save, get, getById, remove }
 }
