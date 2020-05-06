c6677866eba69651e088e150491ebc5ffb4f7406
Felipe <felipe.pereira.dev@gmail.com>
2020-05-06 14:21:10 -0300

    Artigo com Highlight de Código & Responsividade do Menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,83 +11,86 @@
 </template>
 
 <script>
-import axios from 'axios'
-import { baseApiUrl, userKey } from '@/global'
-import { mapState } from 'vuex'
-import Header from './components/template/Header'
-import Menu from './components/template/Menu'
-import Content from './components/template/Content'
-import Footer from './components/template/Footer'
-import Loading from './components/template/Loading'
+import axios from "axios";
+import { baseApiUrl, userKey } from "@/global";
+import { mapState } from "vuex";
+import Header from "./components/template/Header";
+import Menu from "./components/template/Menu";
+import Content from "./components/template/Content";
+import Footer from "./components/template/Footer";
+import Loading from "./components/template/Loading";
 
 export default {
-	name: "App",
-	components: {Header, Menu, Content, Footer, Loading},
-	computed: mapState(['isMenuVisible', 'user']),
-	data: function() {
-		return {
-			validatingToken: true
-		}
-	},
-	methods: {
-		async validateToken(){
-			this.validatingToken = true
-			
-			const json = localStorage.getItem(userKey)
-			const userData = JSON.parse(json)
-			this.$store.commit('setUser', null)
+  name: "App",
+  components: { Header, Menu, Content, Footer, Loading },
+  computed: mapState(["isMenuVisible", "user"]),
+  data: function() {
+    return {
+      validatingToken: true,
+    };
+  },
+  methods: {
+    async validateToken() {
+      this.validatingToken = true;
 
-			if(!userData) {
-				this.validatingToken = false
-				this.$router.push({name: 'auth'})
-				return
-			}
+      const json = localStorage.getItem(userKey);
+      const userData = JSON.parse(json);
+      this.$store.commit("setUser", null);
 
-			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+      if (!userData) {
+        this.validatingToken = false;
+        this.$router.push({ name: "auth" });
+        return;
+      }
 
-			if(res.data) {
-				this.$store.commit('setUser', userData)
-			} else {
-				localStorage.removeItem(userKey)
-				this.$router.push({ name: 'auth'})
-			}
+      const res = await axios.post(`${baseApiUrl}/validateToken`, userData);
 
-			this.validatingToken = false
-		}
-	},
-	created() {
-		this.validateToken()
-	}
-}
+      if (res.data) {
+        this.$store.commit("setUser", userData);
+        if (this.$mq === "xs" || this.$mq === "sm") {
+          this.$store.commit("toggleMenu", false);
+        }
+      } else {
+        localStorage.removeItem(userKey);
+        this.$router.push({ name: "auth" });
+      }
+
+      this.validatingToken = false;
+    },
+  },
+  created() {
+    this.validateToken();
+  },
+};
 </script>
 
 <style>
-	* {
-		font-family: "Lato", sans-serif;
-	}
+* {
+  font-family: "Lato", sans-serif;
+}
 
-	body {
-		margin: 0;
-	}
+body {
+  margin: 0;
+}
 
-	#app {
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-		height: 100vh;
-		display: grid;
-		grid-template-rows: 60px 1fr 40px;
-		grid-template-columns: 300px 1fr;
-		grid-template-areas:
-			"header header"
-			"menu content"
-			"menu footer";
-	}
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
 
-	#app.hide-menu {
-		grid-template-areas: 
-			"header header"
-			"content content"
-			"footer footer";
-	}
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
+}
 </style>
--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -11,92 +11,95 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import Tree from 'liquor-tree'
-import { baseApiUrl } from '@/global'
-import axios from 'axios'
+import { mapState } from "vuex";
+import Tree from "liquor-tree";
+import { baseApiUrl } from "@/global";
+import axios from "axios";
 
 export default {
-  name: 'Menu',
+  name: "Menu",
   components: { Tree },
-  computed: mapState(['isMenuVisible']),
-  data: function(){
+  computed: mapState(["isMenuVisible"]),
+  data: function() {
     return {
-      treeFilter: '',
+      treeFilter: "",
       treeData: this.getTreeData(),
       treeOptions: {
-        propertyNames: {'text': 'name'},
-        filter: { emptyText: 'Categoria não encontrada'}
-      }
-    }
+        propertyNames: { text: "name" },
+        filter: { emptyText: "Categoria não encontrada" },
+      },
+    };
   },
   methods: {
-    getTreeData(){
-      const url = `${baseApiUrl}/categories/tree`
-      return axios.get(url).then(res => res.data)
+    getTreeData() {
+      const url = `${baseApiUrl}/categories/tree`;
+      return axios.get(url).then((res) => res.data);
     },
-    onNodeSelect(node){
+    onNodeSelect(node) {
       this.$router.push({
-        name: 'articlesByCategory',
-        params: { id: node.id }
-      })
-    }
+        name: "articlesByCategory",
+        params: { id: node.id },
+      });
+      if (this.$mq === "xs" || this.$mq === "sm") {
+        this.$store.commit("toggleMenu", false);
+      }
+    },
   },
-  mounted(){
-    this.$refs.tree.$on('node:selected', this.onNodeSelect)
+  mounted() {
+    this.$refs.tree.$on("node:selected", this.onNodeSelect);
   },
-}
+};
 </script>
 
 <style>
-  .menu {
-    grid-area: menu;
-    background: linear-gradient(to right, #232526, #414345);
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-  }
-  .menu a,
-  .menu a:hover {
-    text-decoration: none;
-    color: #fff;
-  }
+.menu {
+  grid-area: menu;
+  background: linear-gradient(to right, #232526, #414345);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+.menu a,
+.menu a:hover {
+  text-decoration: none;
+  color: #fff;
+}
 
-  .menu .tree-node.selected > .tree-content,
-  .menu .tree-node .tree-content:hover {
-    background-color: rgba(255, 255, 255, 0.2);
-  }
+.menu .tree-node.selected > .tree-content,
+.menu .tree-node .tree-content:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+}
 
-  .tree-arrow.has-child {
-    filter: brightness(2);
-  }
+.tree-arrow.has-child {
+  filter: brightness(2);
+}
 
-  .menu .menu-filter {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 20px;
-    padding-bottom: 8px;
-    border-bottom: 1px solid #aaa;
-  }
+.menu .menu-filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #aaa;
+}
 
-  .menu .menu-filter i {
-    color: #aaa;
-    margin-right: 10px;
-  }
+.menu .menu-filter i {
+  color: #aaa;
+  margin-right: 10px;
+}
 
-  .menu input {
-    color: #ccc;
-    font-size: 1.3rem;
-    border: none;
-    outline: none;
-    width: 100%;
-    background: transparent;
-  }
+.menu input {
+  color: #ccc;
+  font-size: 1.3rem;
+  border: none;
+  outline: none;
+  width: 100%;
+  background: transparent;
+}
 
-  .tree-filter-empty {
-    color: #ccc;
-    margin-left: 20px;
-    font-size: 1.3rem;
-  }
+.tree-filter-empty {
+  color: #ccc;
+  margin-left: 20px;
+  font-size: 1.3rem;
+}
 </style>
